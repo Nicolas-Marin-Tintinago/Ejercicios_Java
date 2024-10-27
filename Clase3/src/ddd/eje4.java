3976d0565a5f2113e74dd9930a80ecda87a9838f
Nicolas-Marin-Tintinago <marintintinagonicolas@gmail.com>
2024-10-27 14:23:45 -0500

    Cambio de mensaje

--- Clase3/src/ddd/eje4.java
+++ Clase3/src/ddd/eje4.java
@@ -1,5 +1,56 @@
 package ddd;
 
+
+import java.util.Scanner;
+
+import Clase3.Calculadora;
+import Clase3.Calculadora.* ; 
+
 public class eje4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Intercambio();
+	}
+	
+	public static void mostrar (String mensaje){
+        System.out.println(mensaje);
+    }
+	
+    public static int leerInt (String mensaje){
+        int Numero;
+        Scanner sc = new Scanner(System.in);
+        mostrar(mensaje);
+        Numero = sc.nextInt();
+
+        return Numero;
+    }
+    
+    public static int centena(int valor) {
+    	return valor/100;
+    }
+    
+    public static int decena(int valor) {
+    	return valor/10%10;
+    }
+    
+    public static int unidad(int valor) {
+    	return valor%100%10;
+    }
+    public static int suma(int valor) {
+    	
+    	int suma= unidad(valor)+decena(valor)+centena(valor);
+    	return suma;
+    }
+    public static void Intercambio() {
+    int valor=leerInt("Ingrese el numerooooo");
+    mostrar(""+centena(valor)+" "+decena(valor)+" "+unidad(valor));
+     mostrar(" "+suma(valor));
+    
+     Calculadora clc = new Calculadora(); 
+    }
+    
+    
 
 }
